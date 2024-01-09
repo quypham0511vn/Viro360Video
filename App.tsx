d280df9de99f68c97a0bc0fb6e29bd1beab8ca18
Quy <phamminhquyvn1999@gmail.com>
2024-01-09 13:05:13 +0700

    run

--- App.tsx
+++ App.tsx
@@ -1,40 +1,85 @@
 
-import { Viro360Video } from '@viro-community/react-viro/dist/components/Viro360Video';
-import React from 'react';
+import { Viro360Video, ViroARScene, ViroARSceneNavigator, ViroScene, ViroText, ViroVRSceneNavigator, ViroVideo } from '@viro-community/react-viro';
+import React, { useRef, useState } from 'react';
 import {
+    Dimensions,
     StyleSheet,
     Text,
     View,
 } from 'react-native';
 
-function App(): React.JSX.Element {
+const FirstSceneAR = () => {
 
-    const url = 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/progressive.mp4';
+    const onInitialized = (state: any, reason: any) => { console.log('guncelleme', state, reason); }
+    const onFinish = () => { console.log('end') };
+    const onUpdateTime = () => { 
+        // console.log("Video time update, current: ");
+ };
+    const onVideoError = () => { console.log("Video loading failed with error: "); };
 
-    const onFinish = () => { };
-    const onUpdateTime = () => { };
-    const onVideoError = () => { };
+    const [muted, setMuted] = useState(false);
+    const [volume, setVolume] = useState(1);
+    const ref = useRef(null);
+
+    const handleClickRestartVideo = () => {
+        ref?.current?.seekToTime?.(0);
+    };
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello world</Text>
+        <ViroScene >
             <Viro360Video
-                source={{uri: url}}
+                ref={ref}
+                source={{ uri: 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/progressive.mp4' }}
                 onFinish={onFinish}
                 onUpdateTime={onUpdateTime}
                 onError={onVideoError}
                 loop={true}
                 paused={false}
-                volume={1.0}
+                muted={muted}
+                volume={volume}
             />
+            <ViroText
+                position={[0, -2, -1]}
+                text="Bat dau lai video nay"
+                onClick={handleClickRestartVideo}
+                transformBehaviors={["billboard"]}
+                width={2}
+                height={2}
+                textLineBreakMode="Justify"
+                outerStroke={{ type: "Outline", width: 38, color: '#A1EE42CE' }}
+                style={{
+                    color: "#C42607",
+                    fontFamily: 'Arial',
+                    fontSize: 30,
+                    fontWeight: '400',
+                }}
+            />
+        </ViroScene>
+    );
+};
+
+
+function App(): React.JSX.Element {
+    const w = Dimensions.get('screen').width;
+    const h = Dimensions.get('screen').height;
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ width: w, height: h }}>
+                <ViroVRSceneNavigator initialScene={{
+                    scene: FirstSceneAR
+                }}
+                    autofocus={true}
+                />
+            </View>
         </View>
+
     );
 }
 
 const styles = StyleSheet.create({
-    highlight: {
-        fontWeight: '700',
-    },
+    f1: {
+        flex: 1
+    }
 });
 
 export default App;
